68d06428fc2cb5fc6efb1bbc9cafa8774fbcd02e
chethansd09 <chethansd09@gmail.com>
2022-07-11 15:52:13 +0530

    initial

---
+++ backend/src/main/java/eshop/homedecor/shopapi/entity/ProductCategory.java
@@ -0,0 +1,86 @@
+package eshop.homedecor.shopapi.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+@DynamicUpdate
+public class ProductCategory implements Serializable {
+    @Id
+    @Column(name="category_id")
+    @GeneratedValue
+    private Integer categoryId;
+    
+    @Column(name="category_name")
+    private String categoryName;
+    
+    @Column(name="")
+    @NaturalId
+    private Integer categoryType;
+    
+    @Column(name="")
+    private Date createTime;
+    
+    @Column(name="")
+    private Date updateTime;
+
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Integer getCategoryType() {
+		return categoryType;
+	}
+
+	public void setCategoryType(Integer categoryType) {
+		this.categoryType = categoryType;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+    
+   
+}
